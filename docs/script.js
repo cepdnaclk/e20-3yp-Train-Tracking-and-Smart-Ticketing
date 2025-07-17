c0b6bd90510cb0bcfbaa2a94c0990ccc6a8a5363
Dissanayakanadeeka <e20078@eng.pdn.ac.lk>
2025-07-17 21:12:15 +0530

    update the web page

--- docs/script.js
+++ docs/script.js
@@ -3,16 +3,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     links.forEach(link => {
         link.addEventListener("click", event => {
-            event.preventDefault();
-            const targetId = link.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
+            const href = link.getAttribute("href");
+            if (href && href.startsWith("#")) {
+                event.preventDefault();
+                const targetId = href.substring(1);
+                const targetSection = document.getElementById(targetId);
 
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop - 50,
-                    behavior: "smooth"
-                });
+                if (targetSection) {
+                    window.scrollTo({
+                        top: targetSection.offsetTop - 50,
+                        behavior: "smooth"
+                    });
+                }
             }
         });
     });
 });
+
+
+const reveals = document.querySelectorAll('.reveal, .fade-in-left, .fade-in-right');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show'); // Optional
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+reveals.forEach(el => observer.observe(el));
